5725e7a5b4560535a5e9eef6a66df00d2efa31c2
BarbozaGSB <gustavoo.sbz@gmail.com>
2025-02-23 13:25:09 +0000

    Iniciando os trabalhos

--- CartasSupeTrunfoNovato.c
+++ CartasSupeTrunfoNovato.c
@@ -1,6 +1,29 @@
 #include<stdio.h> 
 int main() { 
-    printf("Cartas Super Trunfo Novato!\n");
+    
+printf("DESAFIO SUPER TRUNFO NOVATO!\n");
+   
+   // O usuário escolherá entre duas cartas disponíveis; 
+
+      char cartaA[10] = "AMAZONAS"; 
+      char B[10] = "BAHIA";
+      char cod[3];
+      char nome[20];
+      int populacao;  
+      int turismo;
+      float area;
+      float pib;
+    
+    // "A" para escolher o estado do AMAZONAS ou "B" para escolher BAHIA; 
+
+        printf("Vamos começar o jogo?\n");
+        printf("Escolha sua primeira carta.\n");
+        printf("Digite A para escolher o estado do AMAZONAS ou B para escolher o estado da BAHIA\n");
+        scanf("%s", cartaA);
+        printf("Você escolheu a carta: %s\n", cartaA);
+          
+
+
 
 return 0;
 }
